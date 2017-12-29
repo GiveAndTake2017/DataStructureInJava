964101c9fdefb41bbd78f2d8b090a1dbb57a0316
Wu, Jiazhuo <wjzlxw2013@163.com>
2017-12-30 00:56:29 +0800

    test change and commit

--- src/Stack/CalculateSuffixExpressionByStack.java
+++ src/Stack/CalculateSuffixExpressionByStack.java
@@ -3,8 +3,7 @@ package Stack;
 public class CalculateSuffixExpressionByStack {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("CalculateSuffixExpressionByStack");
 	}
 
 }
